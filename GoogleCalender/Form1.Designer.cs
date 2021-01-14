f8af55b7a5f7079e8fec7b4279919e0cd4d5d122
Ed Calvert <ed@calvert4.me.uk>
2021-01-14 19:28:48 +0000

    Inital Push

---
+++ GoogleCalender/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace GoogleCalender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DisplayClock = new System.Windows.Forms.Label();
+            this.NextUp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DisplayClock
+            // 
+            this.DisplayClock.BackColor = System.Drawing.Color.Transparent;
+            this.DisplayClock.Font = new System.Drawing.Font("Open Sans", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayClock.ForeColor = System.Drawing.Color.DimGray;
+            this.DisplayClock.Location = new System.Drawing.Point(-5, -5);
+            this.DisplayClock.Name = "DisplayClock";
+            this.DisplayClock.Size = new System.Drawing.Size(91, 42);
+            this.DisplayClock.TabIndex = 2;
+            this.DisplayClock.Text = "00:00";
+            // 
+            // NextUp
+            // 
+            this.NextUp.AutoSize = true;
+            this.NextUp.Location = new System.Drawing.Point(-1, 37);
+            this.NextUp.Name = "NextUp";
+            this.NextUp.Size = new System.Drawing.Size(89, 15);
+            this.NextUp.TabIndex = 3;
+            this.NextUp.Text = "Nothing to do...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(214, 61);
+            this.Controls.Add(this.NextUp);
+            this.Controls.Add(this.DisplayClock);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.5D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Clock";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label DisplayClock;
+        private System.Windows.Forms.Label NextUp;
+    }
+}
+
---
+++ GoogleCalender/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace GoogleCalender
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        public Form1()
+        {
+            
+            InitializeComponent();
+            GoogleAPI();
+        }
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+        private void GoogleAPI()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 1;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+
+            // List events.
+            Events events = request.Execute();
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                NextUp.Text = "";
+                //label2.Text = "";
+                foreach (var eventItem in events.Items)
+                {
+                    NextUp.Text += eventItem.Summary + Environment.NewLine;
+                    // NextUp.Text += eventItem.Description+Environment.NewLine;
+                }
+            }
+            else
+            {
+                NextUp.Text = "Nothing to do";
+            }
+
+            int minute =0;
+            int currentMinute = DateTime.Now.Minute;
+            if (currentMinute > 9)
+                {
+                 minute = int.Parse(DateTime.Now.Minute.ToString().ToArray()[1].ToString());
+            }
+            else
+            {
+                 minute = currentMinute;
+            }
+            DisplayClock.Text = CurrentTime();
+            timer.Interval = 60000- minute*1000;
+            timer.Tick += new EventHandler(this.UpdateClock);
+
+            timer.Start();
+            
+        }
+
+        private void UpdateTextBox()
+        { 
+            
+        }
+
+        private string CurrentTime()
+        {
+            timer.Interval = 60000;
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+
+            string time = "";
+
+            if (hh < 10)
+            {
+                time += "0" + hh;
+
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":";
+            if (mm < 10)
+            {
+                time += "0" + mm;
+
+            }
+            else
+            {
+                time += mm;
+            }
+            return time;
+        }
+
+        private void UpdateClock(object sender, EventArgs e)
+        {
+           
+            DisplayClock.Text = CurrentTime();
+            GoogleAPI();
+
+        }
+
+
+        private void GetEvents_Tick(object sender, EventArgs e)
+        {
+            GoogleAPI();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
