423b67c644e1d54aeea6db86afe44ea1a48630d0
Ed Calvert <ed@calvert4.me.uk>
2021-01-27 08:28:06 +0000

    Doesn't work now!

--- GoogleCalender/Form1.cs
+++ GoogleCalender/Form1.cs
@@ -72,57 +72,67 @@ namespace GoogleCalender
         {
 
             UserCredential credential;
-
-            using (var stream =
-                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            var path = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "credentials.json";
+            InfoMessage(File.Exists(path).ToString(), "Clock.exe", DebugMode);
+            if (true)//File.Exists(path))
             {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
-                string credPath = "token.json";
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-            }
-
-            // Create Google Calendar API service.
-            var service = new CalendarService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-
-            // Define parameters of request.
-            EventsResource.ListRequest request = service.Events.List("primary");
-            request.TimeMin = DateTime.Now;
-            request.ShowDeleted = false;
-            request.SingleEvents = true;
-            request.MaxResults = 1;
-            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
-
-
-            // List events.
-            try
-            {
-                events = request.Execute();
-                UpdateTextBox();
-            }
-            catch (Exception exception)
-            {
-                if (DebugMode)
+                using (var stream =
+                    new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
                 {
-                    WarningMessage("Request could not be fuffiled: " + exception, "Clock.exe");
+                    // The file token.json stores the user's access and refresh tokens, and is created
+                    // automatically when the authorization flow completes for the first time.
+                    string credPath = "token.json";
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
                 }
-                else
-                {
-                    NextUp.Text = "Error occurred, trying again.";
-                }
-                CalibrateAPITimer();
-                CalibrateClockTimer();
-            }
 
+
+                // Create Google Calendar API service.
+                var service = new CalendarService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                // Define parameters of request.
+                EventsResource.ListRequest request = service.Events.List("primary");
+                request.TimeMin = DateTime.Now;
+                request.ShowDeleted = false;
+                request.SingleEvents = true;
+                request.MaxResults = 1;
+                request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+
+                // List events.
+                try
+                {
+                    events = request.Execute();
+                    UpdateTextBox();
+                }
+                catch (Exception exception)
+                {
+                    if (DebugMode)
+                    {
+                        WarningMessage("Request could not be fuffiled: " + exception, "Clock.exe");
+                    }
+                    else
+                    {
+                        NextUp.Text = "Error occurred, trying again.";
+                    }
+                    CalibrateAPITimer();
+                    CalibrateClockTimer();
+                }
+            }
+            else
+            {
+                UpdateEventText("Error: credentials.json doesn't exist.", 'o');
+                DisplayClock.Text = "00:00";
+            }
 
 
 
